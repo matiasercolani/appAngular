29c13a331cd7d811c56ac30e05393cd30ff571d3
matiasercolani <matiaserc514@gmail.com>
2021-03-18 21:29:25 -0300

    1er clase, ejercicio1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'appAngular';
+
+  title = 'Lab4';
+
+  cambiarTitulo(){
+    this.title = "erco";
+  }
+
+  // Ejercicio Clase 1
+
+  edad1:number = 0;
+  edad2:number = 0;
+  resultadoPromedio:number = 0;
+  resultadoSuma:number = 0;
+  
+  promedioSuma(){
+    this.resultadoSuma = this.edad1 + this.edad2;
+    this.resultadoPromedio = this.resultadoSuma / 2;
+  }
+
+  borrar(){
+    this.edad1 = 0;
+    this.edad2 = 0;
+    this.resultadoPromedio = 0;
+    this.resultadoSuma = 0;
+  }
+
 }
